95a7cee73cf46a8b22b62bfeb9947cdd77d51e4a
Sebastian Jeong <sebastianrcnt@gmail.com>
2021-05-30 22:12:54 +0900

    [feat] refactor wss

--- server.js
+++ server.js
@@ -5,21 +5,77 @@ const http = require("http");
 const app = express();
 const server = http.createServer(app);
 
-const wss = new WebsocketServer({ server });
+// Websocket Config
+const websocketServer = new WebsocketServer({ server });
+const shortid = require("shortid");
 
-wss.on('connection', (ws) => {
-    ws.on('message', (raw) => {
-        console.log('raw', JSON.parse(raw))
-        wss.clients.forEach((client) => {
-            if (client !== ws) {
-                client.send(raw)
-            }
+// on-memory database(?)
+const CLIENTS = {}; // map(dictionary)
+const ROOMS = {};
+
+websocketServer.on("connection", (socket) => {
+  // Connection 시작됐을때
+  socket.id = shortid.generate();
+  CLIENTS[socket.id] = socket;
+
+  socket.send(`Connection Established`);
+  console.log(`Connection Established for ${socket.id}`);
+
+  // Connection 끝났을때
+  socket.on("close", () => {
+    console.log(`Connection Terminated for ${socket.id}`);
+    delete CLIENTS[socket.id];
+  });
+
+  // Message Parsing을 한 후 응답 혹은 예외처리
+  socket.on("message", (rawMessageData) => {
+    let messageData;
+    try {
+      messageData = JSON.parse(rawMessageData);
+    } catch (error) {
+      socket.send(
+        JSON.stringify({
+          success: false,
+          reason: "JSON Parse Failed",
+          error,
         })
-    })
-    console.log('connection!')
-    ws.send("hi i am websocket server")
-})
+      );
+    }
+
+    handleMessage(socket, messageData);
+  });
+});
+
+/*
+type MessageData {
+  type: string;
+  payload: any;
+}
+*/
+
+function handleMessage(client, messageData) {
+  switch (messageData.type) {
+    case "broadcast":
+      MessageHandlers.broadcast(client, messageData.payload);
+      break;
+    default:
+      client.send(JSON.stringify({
+        success: false,
+        reason: `Invalid Action type ${messageData.type}`
+      }))
+  }
+}
+
+const MessageHandlers = {
+  broadcast(currentClient, payload) {
+    websocketServer.clients.forEach((client) => {
+      if (client !== currentClient) {
+        client.send(JSON.stringify(payload));
+      }
+    });
+  }, 
+};
 
 server.listen(3000, () => {
-    console.log('server started')
-})
+  console.log("[Server Started]");
+});
